5a79b75a7e8c5add2d2ac8f32a255c50241df08b
Gristly Sid <gristlysid@gmail.com>
2022-12-13 12:43:19 +0500

    First Commit

---
+++ Todo List/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',()=>{
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el =  document.querySelector("#tasks");
+
+    console.log(form);
+})
