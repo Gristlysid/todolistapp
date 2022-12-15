f929e8fc7f8a1fa9cdc73c045f5f6d46a7ba6bf7
Gristly Sid <gristlysid@gmail.com>
2022-12-15 10:02:44 +0500

    List updation is ready

--- Todo List/main.js
+++ Todo List/main.js
@@ -3,5 +3,36 @@ window.addEventListener('load',()=>{
     const input = document.querySelector("#new-task-input");
     const list_el =  document.querySelector("#tasks");
 
-    console.log(form);
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+   const task=input.value;
+
+   if (!task) {
+        alert("please fill out the task");
+        return;
+   }
+// Making a div with a class task
+   const task_el = document.createElement("div");
+   task_el.classList.add("task");
+
+// Making a div with a class task
+   const task_content_el = document.createElement("div");
+   task_content_el.classList.add("content");
+
+
+   task_el.appendChild(task_content_el);
+
+// Making a input element with a class some text
+   const task_input_el = document.createElement("input");
+   task_input_el.classList.add("text");
+   task_input_el.type = "text";
+   task_input_el.value = task;
+   task_input_el.setAttribute("readonly", "readonly");
+
+   task_content_el.appendChild(task_input_el);
+   
+   list_el.appendChild(task_el);
+  })
+
 })
